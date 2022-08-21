3bd62ae7ccb9f4f52cb10ccfa637dfb08eeffe50
kw <kw.15@hotmail.com>
2022-08-21 21:28:47 +0800

    finallllllly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,38 +10,10 @@ public class Main {
 
         System.out.println("Origin graph:");
         graph.printGraph();
-        graph.purge();
-        System.out.println("after purged:");
-        graph.printGraph();
 
         CycleFinder cycleFinder = new CycleFinder();
         List<Cycle> cycles = cycleFinder.findCycles(graph);
         cycles.forEach(System.out::println);
-
-//        Node a = new Node("A");
-//        Node b = new Node("B");
-//        Node c = new Node("C");
-//        Node d = new Node("D");
-//        Node e = new Node("E");
-//        Node f = new Node("F");
-//        Node g = new Node("G");
-//
-//        a.link(b, c, d);
-//        b.link(f);
-//        c.link(a);
-//        d.link(e);
-//        e.link(a, d, g);
-//        f.link(b);
-//        g.link(a);
-//
-//        Graph graph = new Graph(a, b, c, d, e, f, g);
-//        graph.printGraph();
-//
-//        Set<Cycle> allCycles = graph.findAllCycles();
-//
-//        Streams.mapWithIndex(allCycles.stream(),
-//                        (str, index) -> (index+1) + ": " + str)
-//                .forEach(System.out::println);
 
     }
 
